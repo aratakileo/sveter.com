8974eb667b21f5e49ee330e8c2118ef7da0f08a8
TeaCondemns <tea.condemns@gmail.com>
2023-04-10 14:46:14 +0300

    Base preparings

--- app/src/main/java/ru/piece/of/crown/sveter/com/MainActivity.kt
+++ app/src/main/java/ru/piece/of/crown/sveter/com/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.piece.of.crown.sveter.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var userAvatar: ImageView
+    lateinit var userName: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userAvatar = findViewById(R.id.userAvatar)
+        userName = findViewById(R.id.userName)
     }
 }
