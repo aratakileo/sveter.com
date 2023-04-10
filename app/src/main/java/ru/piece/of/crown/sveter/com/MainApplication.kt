1fe45b5dcce119ba9ee8f65cf916ccae6a17c9cb
TeaCondemns <tea.condemns@gmail.com>
2023-04-10 12:25:34 +0300

    Initial commit

---
+++ app/src/main/java/ru/piece/of/crown/sveter/com/MainApplication.kt
@@ -0,0 +1,12 @@
+package ru.piece.of.crown.sveter.com
+
+import android.app.Application
+import com.yandex.mapkit.MapKitFactory
+
+class MainApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        MapKitFactory.setApiKey(resources.getString(R.string.yandexMapAPIToken))
+    }
+}
